e52fbaaeae38f9b1b66db856bc7e7dd40f42d2b1
Prathvi-co <shettyprathviraj784@gmail.com>
2025-08-20 20:46:43 +0530

    Fix: Add CORS with FRONTEND_URL

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './authContext'; // <-- IMPORT AuthProvider
+console.log("API URL:", process.env.REACT_APP_API_URL);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
